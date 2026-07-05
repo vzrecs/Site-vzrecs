2d59539a319bd9636c2d09eb1978bf33aaf9c9a7
Sebastian <vz.recs@gmail.com>
2026-07-05 16:21:11 -0300

    Atualiza favicon com fundo preto

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
       { url: "/favicon.png", type: "image/png", sizes: "512x512" }
     ],
+    shortcut: [{ url: "/favicon.ico" }],
     apple: [{ url: "/apple-touch-icon.png", type: "image/png", sizes: "180x180" }]
   }
 };
